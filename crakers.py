9cc9308a08ebff561c11ce0d95b7277e51f51de9
Naor Movshovitz <nmovshov@gmail.com>
2018-11-16 21:30:55 -0800

    again

--- crakers.py
+++ crakers.py
@@ -1,12 +1,5 @@
 # practice problems
 import sys
-import data
-
-def test():
-    print("alo world")
-    head = data.LinkedListNode()
-    print(head.value)
 
 if __name__ == "__main__":
-    test()
     pass
